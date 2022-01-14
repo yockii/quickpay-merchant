af3dcc14732e78e456952ac9104aadbde3248271
yockii <xuyuqi@gmail.com>
2022-01-14 18:35:18 +0800

    路由守卫

--- src/router/index.js
+++ src/router/index.js
@@ -26,5 +26,13 @@ export default route(function (/* { store, ssrContext } */) {
     history: createHistory(process.env.MODE === 'ssr' ? void 0 : process.env.VUE_ROUTER_BASE)
   })
 
+  Router.beforeEach((to, from, next) => {
+    if (to.path !== '/login' && !store.state.user.token) {
+      next('/login')
+    } else {
+      next()
+    }
+  })
+
   return Router
 })
